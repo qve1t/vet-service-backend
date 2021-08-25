b612081f4ed10b65bd3e0030753fcc62af12aa3d
qve1t <warpas.piotr@gmail.com>
2021-08-25 17:31:23 +0200

    enabled implicit conversions on requests

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   app.use(cookieParser());
   app.enableCors({ origin: 'http://localhost:3001', credentials: true }); // change before building production
   await app.listen(3000);
